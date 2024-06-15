220cca0af2528c9bd7c41cb3aa01adf6b687bb39
vector-10 <duzieblaise10@gmail.com>
2024-06-15 09:23:10 -0400

    chore:updates

--- app/page.js
+++ app/page.js
@@ -7,12 +7,15 @@ export default function Home() {
     <main className="overflow-y-auto min-h-screen flex flex-col bg-[#0b1120]">
       <Header />
       <section className="hero-section flex flex-col text-center justify-center mt-[12rem]">
-        <h1 className="text-5xl text-white font-bold">Stake and and Earn Yields on Frysk <br/> built on the L2 frax chain .</h1>
+        <h1 className="text-5xl text-white font-bold">Stake and and Earn Yields on Frysk <br/> built on the L2 frax chain.</h1>
         <p className="text-gray-300 mt-4 text-md regular">A filtered study application and knowledge base <br /> through Artificial Intelligence built to arm you with core concepts to Ace your JAMB examinations.</p>        
       </section>      
       <div className="action-buttons flex justify-center mt-10 text-md">
       <button className="text-white border rounded-sm py-2 px-5 mx-3 cursor-pointer transition duration-300 ease-in-out hover:text-blue-400">
       Connect Wallet
+      </button>           
+      <button className="text-[#0b1120] font-bold bg-white border rounded-sm py-2 px-5 mx-3 cursor-pointer transition duration-300 ease-in-out hover:text-blue-400">
+      Explore Markets
       </button>           
     
       </div>
--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
   // define all state here
     const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='fixed w-full px-[2rem] h-[4rem] flex items-center justify-between shadow-md bg-[#1c2337]'>
+    <div className='fixed w-full px-[2rem] h-[4rem] flex items-center justify-between shadow-md border-white'>
          {/* Hamburger Menu for mobile devices*/}
          <button 
             className='md:hidden text-white transition duration-500 ease-in-out' 
@@ -18,21 +18,22 @@ const Header = () => {
         >
             {isOpen ? <AiOutlineClose fontSize={25} /> : <AiOutlineMenu fontSize={25} />}
         </button>
-        <div className="logo-name wiggly text-4xl text-white hover:scale-75 hover:rotate-12 cursor-pointer transition duration-500 ease-in-out">
-         Frysk
-        </div>        
-        {/* end of div 1 and start of div 2 */}
-        <div className='app-links hidden md:flex justify-between w-[36rem] items-center text-lg'>
-            <div className='flex bg-white text-[#1c2337] py-0.5 px-1.5 rounded-2xl space-x-2'>
-                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-1 px-2.5" href="/dashboard">Home</Link>
-                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-1 px-2.5" href="/quiz">Vault</Link>
-                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-1 px-2.5" href="/pdf">Bridge</Link>
-                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-1 px-2.5" href="/bot">Bot</Link>
+        <div className="logo-name wiggly flex text-white cursor-pointer space-x-6">
+        <h1 className='hover:scale-75 hover:rotate-12 transition duration-500 ease-in-out text-4xl'>Frysk</h1> 
+
+         <div className='flex bg-white text-[#1c2337] py-1 px-2 rounded-2xl space-x-2'>
+                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-0.5 px-2.5" href="/dashboard">Home</Link>
+                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-0.5 px-2.5" href="/quiz">Vault</Link>
+                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-0.5 px-2.5" href="/pdf">Bridge</Link>
             </div>
 
+        </div>        
+        {/* end of div 1 and start of div 2 */}
+        <div className='app-links hidden md:flex justify-between  items-center text-lg'>
+           
            <div className='flex items-center text-lg'>
-            <Link className='wiggly border mx-4 text-white py-2 px-5 transition duration-500 ease-in-out hover:text-blue-600' href="/test">Stake</Link>
-            <div className='flex bg-white text-[#1c2337] py-0.5 px-1.5 rounded-2xl space-x-2 '>
+            <Link className='wiggly border mx-4 text-white py-1.5 px-5 transition duration-500 ease-in-out hover:text-blue-600' href="/test">Stake</Link>
+            <div className='flex bg-white text-[#1c2337] py-0.5 px-1.5 rounded-2xl space-x-2 iuk'>
                 <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-xl py-0.5 px-2.5 " href="/steeze-board">Connect Wallet</Link>
             </div>
            </div>
