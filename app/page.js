a7b27e3cf94d3c682eec69a77341bc4be69397b2
vector-10 <duzieblaise10@gmail.com>
2024-06-27 14:56:58 +0100

    chore:updates

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
     <div className="overflow-y-auto min-h-screen flex flex-col ">      
       <section className="hero-section flex flex-col text-center justify-center mt-[16rem]">
         <h1 className=" text-4xl md:text-5xl text-white font-bold "><span className='text-[#FFB07C]'>Stake</span> and Earn Yields on <span className='text-[#FFB07C]'>Frysk</span> <br/> built on the L2 frax chain.</h1>
-        <p className="text-gray-300 mt-4 text-xs md:text-lg regular">Frysk enables you to Stake and Earn Maximun Yields <br /> through the Frax Protocol on the L2 Frax Blockchain in a seamless, pain free process.</p>        
+        <p className="text-gray-300 mt-4 text-xs md:text-lg">Frysk enables you to Stake and Earn Maximun Yields <br /> through the Frax Protocol on the L2 Frax Blockchain in a seamless, pain free process.</p>        
       </section>      
       <div className="action-buttons flex justify-center mt-10 text-lg space-x-4 wiggly">
         <button className="text-white border rounded-sm py-2 px-2 cursor-pointer transition duration-300 ease-in-out hover:text-blue-400 flex items-center space-x-2">
@@ -27,7 +27,7 @@ export default function Home() {
         Explore Markets
         </button>        
       </div>
-      <div class="grid grid-cols-3 divide-x text-lg mt-[10rem] regular text-center border mx-[4rem] md:mx-[14rem] h-[7rem] bg-black text-white rounded-lg shadow-lg shadow-[#ffb07c9e]">
+      <div class="grid grid-cols-3 divide-x text-lg mt-[10rem] wiggly text-center border mx-[4rem] md:mx-[14rem] h-[10rem] bg-black text-white rounded-lg shadow-lg shadow-[#ffb07c9e]">
           <div className='flex flex-col justify-center items-center rounded-sm cursor-pointer '>           
            <p className='text-xs'>Trading Volume</p>
            <p className='font-bold wiggly md:text-4xl text-md'>3,000,000</p>
@@ -47,7 +47,7 @@ export default function Home() {
 
         <section className='flex flex-col justify-center items-center text-white mt-[10rem] mb-[10rem] font-bold text-center'>
           <h1 className='text-5xl'> <span className='text-[#FFB07C]'>Surplus</span> Yields <br/> <span className='text-[#FFB07C]'>Maximum</span> Stakes!</h1>
-          <p className='regular mt-[2rem]'> Featuring a variety of assets from across EVM and Cosmos, with new assets added regularly</p>
+          <p className='wiggly mt-[2rem]'> Featuring a variety of assets from across EVM and Cosmos, with new assets added wigglyly</p>
         </section>
     </div>
 
@@ -60,11 +60,11 @@ export default function Home() {
       </div>
       <div className='text flex flex-col justify-center items-center'>
         <h1 className='text-5xl font-bold mb-4 mt-16 '>Lock up Your <br /> <span className='text-[#FFB07C]'>Assets in Vaults</span> </h1>
-        <p className='regular text-sm'>Obtain Maximum returns per Hour, Increase earning potential <br /> and ensured stability with our stable pairs</p>
+        <p className='wiggly text-sm'>Obtain Maximum returns per Hour, Increase earning potential <br /> and ensured stability with our stable pairs</p>
       </div>
     </div>
 
-    <footer className='text-white mt-[4rem] bg-black h-[12rem] regular'>
+    <footer className='text-white mt-[4rem] bg-black h-[12rem] wiggly'>
       <div className='border-b flex flex-col md:flex-row justify-around items-center h-[9rem] '>
         <div className='logo-name'>
          <h1 className='hover:scale-75 transition duration-500 ease-in-out text-4xl wiggly'>Frysk</h1>
--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div className='fixed w-full px-[2rem] h-[4rem] flex items-center justify-between border-b bg-black shadow-md shadow-[#ffb07c9e]'>
+    <div className='fixed w-full px-[2rem] h-[4rem] flex items-center justify-between border-b  bg-black shadow-md shadow-[#ffb07c9e]'>
          {/* Hamburger Menu for mobile devices*/}
          <button 
             className='md:hidden text-white transition duration-500 ease-in-out' 
@@ -18,13 +18,13 @@ const Header = () => {
         >
             {isOpen ? <AiOutlineClose fontSize={25} /> : <AiOutlineMenu fontSize={25} />}
         </button>
-        <div className="logo-name wiggly flex text-white cursor-pointer space-x-4">
+        <div className="logo-name flex text-white cursor-pointer space-x-4">
         <h1 className='hover:scale-75 hover:rotate-12 transition duration-500 ease-in-out text-4xl'>Frysk</h1> 
 
-         <div className='md:flex bg-white text-[#1c2337] py-1 px-2 rounded-md space-x-2 hidden'>
-                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-md py-0.5 px-2.5" href="/dashboard">Home</Link>
-                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-md py-0.5 px-2.5" href="/quiz">Vault</Link>
-                <Link className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-md py-0.5 px-2.5" href="/pdf">Bridge</Link>
+         <div className='md:flex text-white py-1 px-2 rounded-md space-x-8 hidden'>
+                <Link className="transition duration-500 ease-in-out hover:bg-[#161c2c] py-1 px-2 hover:text-white rounded-md" href="/">Home</Link>
+                <Link className="transition duration-500 ease-in-out hover:bg-[#161c2c] py-1 px-2 hover:text-white rounded-md" href="/vault">Vault</Link>
+                <Link className="transition duration-500 ease-in-out hover:bg-[#161c2c] py-1 px-2 hover:text-white rounded-md" href="/bridge">Bridge</Link>
             </div>
 
         </div>        
@@ -34,7 +34,7 @@ const Header = () => {
            <div className='flex items-center text-lg space-x-4'>
             <Link className='wiggly border text-white py-1.5 px-5 transition duration-500 ease-in-out hover:text-blue-600' href="/test">Stake</Link>
             <div className='flex bg-white text-[#1c2337] py-1 px-1.5 rounded-md space-x-2 '>
-                <button className="wiggly transition duration-500 ease-in-out hover:bg-[#1c2337] hover:text-white rounded-md py-0.5 px-2.5 flex items-center space-x-2" ><p>Connect Wallet</p> <SiWalletconnect /> </button>
+                <button className="wiggly transition duration-500 ease-in-out hover:bg-black  hover:text-white rounded-md py-0.5 px-2.5 flex items-center space-x-2" ><p>Connect Wallet</p> <SiWalletconnect /> </button>
             </div>
            </div>
         </div>
